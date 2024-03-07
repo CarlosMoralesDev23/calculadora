233f30d5327ec2a2da2a3514cf483e4bbd9caad7
CarlosMoralesDev23 <carlosmoralesdev23@gmail.com>
2024-03-07 01:40:07 -0300

    Se crea DOM de numeros - igual - punto

--- src/calculadora/usecases/dom.js
+++ src/calculadora/usecases/dom.js
@@ -5,7 +5,6 @@ const h1 = document.createElement('h1')
 h1.append('Mi Calculadora!')
 
 
-
 const divCalculadora = document.createElement('div')
 divCalculadora.id = 'divCalculadora'
 
@@ -18,6 +17,7 @@ divCalculadoraSuperior.append(divScreen)
 const divCalculadoraInferior = document.createElement("div");
 const divCalculadoraInferiorIzquierdo = document.createElement("div");
 const divNumbers = document.createElement("div");
+divNumbers.id = "divNumbers"
 divCalculadoraInferiorIzquierdo.append(divNumbers)
 
 const divCalculadoraInferiorDerecho = document.createElement("div");
@@ -25,16 +25,32 @@ const divOperators = document.createElement("div");
 const divSimbols = document.createElement("div");
 divCalculadoraInferiorDerecho.append(divOperators, divSimbols)
 
+divCalculadoraInferior.append(divCalculadoraInferiorIzquierdo, divCalculadoraInferiorDerecho)
+
 divCalculadora.append( divCalculadoraSuperior, divCalculadoraInferior )
 
 dom.append(h1, divCalculadora);
 
 
+
 for (let i = 0; i < 10; i++) {
 
     const divNumber = document.createElement('div')
+    divNumber.innerText = `${i}` 
     divNumber.classList.add('divNumber')
     divNumber.id = `divNumber#${i}`
+    divNumbers.append(divNumber)
+
+    if( i === 9) {
+        const divIgual = document.createElement("div");
+        const divPunto = document.createElement("div");
+        divIgual.innerText = `=`;
+        divPunto.innerText = `.`;
+        divIgual.classList.add("divIgualYPunto");
+        divPunto.classList.add("divIgualYPunto");
+        divNumbers.append(divIgual, divPunto);
+    }
+    
 
 }
 
